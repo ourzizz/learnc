b27cf0c5a0cc7fbbd4bab6d3d0217aef565f82e3
ourzizz <552716481@qq.com>
2015-12-10 11:28:00 +0800

    ps02表正确填写

--- vba/ludiaokeps.vb
+++ vba/ludiaokeps.vb
@@ -5,7 +5,6 @@ sub ps0()
     sheet4.cells(11,7)=sheet1.cells(3,5)'单位层次
     sheet4.cells(11,17)=sheet1.cells(3,6)'单位技术领域
 end sub
-
 Function GetAge(StrAge As String)
     StrAge = Mid(StrAge, 1, 4)
     GetAge = 2016 - Val(StrAge)
@@ -94,70 +93,59 @@ Function fillExcel(ps02_row As Integer, ps02() As Integer)
     Next
 End Function
 
-'-------------------------------------------------------------------------------------------------------------------
-Function condition(ps02() as Integer,ObjectRow as integer,sheet2_col as Integer,ifcondition as string)
-    dim sheet2_row as Integer
+Function condition(ps02() As Integer, ObjectRow As Integer, condtionColA As Integer, conditionStrA As String, condtionColB As Integer, conditionStrB As String)
+    '为了if第一个条件也能默认成立，在sheet2的AH一列只要有数据的全置为1，好些默认条件condtionColA=34 string=1
+    Dim sheet2_row As Integer
     sheet2_row = 3
     InitPsArray ps02
     While Sheet2.Cells(sheet2_row, 2) <> ""
-        if sheet2.cells(sheet2_row, sheet2_col) = ifcondition  then
+        If Sheet2.Cells(sheet2_row, condtionColA) = conditionStrA And Sheet2.Cells(sheet2_row, condtionColB) <> conditionStrB Then 'conditionStrB默认设置为2也就是名字列，conditionStrB默认设置为空,那么无论如何第二个条件默认都成立
             Call FillPsArray(sheet2_row, ps02)
-        end if
+        End If
         sheet2_row = sheet2_row + 1
     Wend
     Call fillExcel(ObjectRow, ps02)
-end Function
-'-------------------------------------------------------------------------------------------------------------------
+End Function
 
 Sub ps02Main()
     Dim ps02(32) As Integer
     Dim sheet2_row As Integer
 
-    '-------------------------------------------------------------------------------------------------------------------
-    sheet2_row = 3
-    InitPsArray ps02
-    While Sheet2.Cells(sheet2_row, 2) <> ""
-        Call FillPsArray(sheet2_row, ps02)
-        sheet2_row = sheet2_row + 1
-    Wend
-    Call fillExcel(11, ps02)
-    '-------------------------------------------------------------------------------------------------------------------
+    Call condition(ps02, 11, 34, "", 2, "") '总计
+    Call condition(ps02, 12, 34, "", 12, "") '管理人员小计
+    Call condition(ps02, 13, 4, "女", 2, "")
+    Call condition(ps02, 14, 5, "少数民族", 12, "")
+    Call condition(ps02, 15, 12, "一级职员岗位（部级正职）", 2, "")
+    Call condition(ps02, 16, 12, "二级职员岗位（部级副职）", 2, "")
+    Call condition(ps02, 17, 12, "三级职员岗位（厅级正职）", 2, "")
+    Call condition(ps02, 18, 12, "四级职员岗位（厅级副职）", 2, "")
+    Call condition(ps02, 19, 12, "五级职员岗位（处级正职）", 2, "")
+    Call condition(ps02, 20, 12, "六级职员岗位（处级副职）", 2, "")
+    Call condition(ps02, 21, 12, "七级职员岗位（科级正职）", 2, "")
+    Call condition(ps02, 22, 12, "八级职员岗位（科级副职）", 2, "")
+    Call condition(ps02, 23, 12, "九级职员岗位（科员）", 2, "")
+    Call condition(ps02, 24, 12, "十级职员岗位（办事员）", 2, "")
+    Call condition(ps02, 25, 12, "其他等级人员", 2, "")
 
+    Call condition(ps02, 26, 34, "", 13, "") '专技小计
+    '"-----{
+    { 在管理岗位工作的无法用函数解决
     sheet2_row = 3
     InitPsArray ps02
     While Sheet2.Cells(sheet2_row, 2) <> ""
-        If Sheet2.Cells(sheet2_row, 12) <> "" Then
+        If Sheet2.Cells(sheet2_row, 12) <> "" And Sheet2.Cells(sheet2_row, 13) <> "" Then
             Call FillPsArray(sheet2_row, ps02)
         End If
         sheet2_row = sheet2_row + 1
     Wend
-    Call fillExcel(12, ps02)
-    InitPsArray ps02
-    '-------------------------------------------------------------------------------------------------------------------
+    Call fillExcel(27, ps02)
+    ''}}}}--------------------
 
-    sheet2_row = 3
-    InitPsArray ps02
-    While Sheet2.Cells(sheet2_row, 2) <> ""
-        if sheet2.cells(sheet2_row, 12) <> "" and  sheet2.cells(sheet2_row, 4) <> "男" then
-            Call FillPsArray(sheet2_row, ps02)
-        End If
-        sheet2_row = sheet2_row + 1
-    Wend
-    Call fillExcel(13, ps02)
-    '------------------------------------管理岗位的少数民族统计-------------------------------------------
-    sheet2_row = 3
-    InitPsArray ps02
-    While Sheet2.Cells(sheet2_row, 2) <> ""
-        if sheet2.cells(sheet2_row, 12) <> "" and  sheet2.cells(sheet2_row, 5)  <> "汉族" then
-            Call FillPsArray(sheet2_row, ps02)
-        End If
-        sheet2_row = sheet2_row + 1
-    Wend
-    Call fillExcel(13, ps02)
-    '-------------------------------------------------------------------------------------------------------------------
-    call condition(ps02,15,12,"一级职员岗位（部级正职）")
+    Call condition(ps02, 28, 34, "", 15, "") '具有职业资格的
+    Call condition(ps02, 29, 4, "女", 13, "") '专业技术女
+    Call condition(ps02, 30, 5, "少数民族", 13, "") '专业技术少数民族
+    Call condition(ps02, 31, 13, "高级岗位一级", 2, "")
+    Call condition(ps02, 32, 13, "高级岗位二级", 2, "")
+
 
 End Sub
-
-
-
