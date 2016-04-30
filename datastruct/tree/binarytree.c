cc0e08585d086a856dbd9bfecb8772745334c2b3
ourzizz <552716481@qq.com>
2016-04-30 13:42:44 +0800

    btree429

--- datastruct/tree/binarytree.c
+++ datastruct/tree/binarytree.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
-typedef int ElemenType;
+#include <malloc.h>
+typedef char ElemenType;
 typedef struct BiTNode{
     ElemenType element;
     struct BiTNode * Lchild,*Rchild;
 }BiTNode,* BiTree;
 
+void InitNode(BiTree T)
+{
+    T->element='1';
+    T->Rchild=T->Lchild=NULL;
+}
 int CreateBiTree(BiTree T)
 {//create bitree by preorder input 
-    printf("Create Binarytree by preoder input\n");
-    printf("Enter '#' to finish input ,every time just one number can be inputed\n");
-    printf("Get Start\n");
-    
-
+    char element;
+    printf("please input a char to fill node\n") ;
+    scanf("%c",&element);
+    if (element == ' ') T=NULL;
+    else
+    {
+        if (!(T=(BiTree)malloc(sizeof(BiTNode)))) return 0;
+        T->element=element;
+        CreateBiTree(T->Lchild);
+        CreateBiTree(T->Rchild);
+    }
+    return 1;
 }
+
 int PreOrderTraverse(BiTree T,int (* Vist)(ElemenType e ));
 int InOrderTraverse(BiTree T,int (* Vist)(ElemenType e ));
 int PostOrderTraverse(BiTree T,int (* Vist)(ElemenType e ));
 int LevelOrderTraverse(BiTree T,int (* Vist)(ElemenType e ));
 int main(void)
 {
+    printf("Create Binarytree by preoder input\n");
+    printf("Enter '#' to finish input ,every time just one number can be inputed\n");
+    printf("Get Start\n");
     BiTNode T;
     CreateBiTree(&T);
     return 0;
