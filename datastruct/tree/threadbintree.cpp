62478d06f7669ac0bbf740f2598423b085f0fcce
ourzizz <552716481@qq.com>
2016-05-07 17:15:47 +0800

    写了线索二叉树注释

--- datastruct/tree/threadbintree.cpp
+++ datastruct/tree/threadbintree.cpp
@@ -1,3 +1,10 @@
+/*************************************************
+ * 为了将来的自己还是花点时间写注释吧
+ * 这个文件是手工输入二叉树的前序序列建立线索二叉树
+ * 然后再通过线索中序遍历二叉树的算法
+ * 例如输入AB00C00
+ * 遍历结果为BAC
+**************************************************/
 #include <stack>
 #include <stdio.h>
 #include <malloc.h>
@@ -8,6 +15,8 @@ typedef char ElemenType;
 
 enum  PointerTag{Link,Thread};
 
+// |lchild|ltag|data|rtag|rchild|
+//   Thread || link
 typedef struct BiThrNode{
     ElemenType element;
     struct BiThrNode *lchild,*rchild;
@@ -15,7 +24,7 @@ typedef struct BiThrNode{
 }BiThrNode,*BiThrTree;
 
 int CreateBiTree(BiThrTree *T)
-{//create BiThrTree by preorder input
+{//输入前序序列，递归地建立二叉树，这里的二叉树还不具备线索，只是有存储线索的空间
     char element;
     printf("please input a char to fill node\n") ;
     std::cin >> element;
@@ -31,14 +40,14 @@ int CreateBiTree(BiThrTree *T)
 }
 
 int InOrderTraverse_Thr(BiThrTree T,int (* Vist )(ElemenType e))
-{
+{//中序遍历线索二叉树，起始点为二叉树的头结点，这个结点是在线索化的过程中加入的
     BiThrTree p = T->lchild;
     while(p != T) {
         while(p->LTag==Link) {
             p=p->lchild;
         }
         Vist(p->element);
-        while(p->RTag == Thread && p->rchild!=T) {
+        while(p->RTag == Thread && p->rchild!=T) {//右孩子为线索的话，这个线索就是ｐ的后继，那么ｐ就ｋｅｙ６ｉ跳到后继输出后继然后继续循环，直到ｐ回到头结点
             p = p->rchild;
             Vist(p->element);
         }
@@ -48,7 +57,8 @@ int InOrderTraverse_Thr(BiThrTree T,int (* Vist )(ElemenType e))
 }
 
 void InThreading(BiThrTree p,BiThrTree *pre) 
-{
+{//pre必须作为二级指针传入递归函数，否则在退栈后pre的值就得不到保持，回退为上级栈中的值
+ //p和ｔ交替走指针，靠函数递归进行指针ｐ的回溯,算法想不起来可以参考自考网动画
     if (p!=NULL) {
         InThreading(p->lchild,pre);
         if (!p->lchild) { 
@@ -65,7 +75,7 @@ void InThreading(BiThrTree p,BiThrTree *pre)
 }
 
 BiThrTree InOrderThreading(BiThrTree &Thrt,BiThrTree T)
-{
+{//这个函数只是为InThreading函数做铺垫和善后的，没什么难度，主要是引入头结点,并返回头结点作为遍历函数的入口
     BiThrTree pre=NULL;
     if (!(Thrt = (BiThrTree)malloc(sizeof(BiThrNode))))  return 0;
     Thrt->LTag = Link; Thrt->RTag = Thread;
@@ -95,7 +105,6 @@ int main()
     printf("Create Binarytree by preoder input\n");
     printf("Enter '#' to finish input ,every time just one number can be inputed\n");
     printf("Get Start\n");
-
     BiThrTree T=NULL;
     BiThrTree Temp=NULL;
     CreateBiTree(&T);
