8000bc151ccdd22edd3fc4480f5e98559e4723f1
ourzizz <552716481@qq.com>
2017-06-02 10:57:55 +0800

    公务员选调生警察分两天面试分配考场，可以提供决策结果了

--- vba/mianshifenpei.py
+++ vba/mianshifenpei.py
@@ -3,18 +3,22 @@
 #此脚本功能类似合并尾考场，可多次使用背包算法
 #将每个考场尽最大量填充
 #公务员、警察、选调生分别执行脚本
-#数据库表结构ksinfo:ksxm/zkzh/sfzh/bkdw/bkzw/leixing
-#            zhiwei:dwmc/zwmc/leixing/rs/kcxh
+#数据库表结构ksinfo:ksxm/zkzh/sfzh/bkdw/bkzw/leixing/minzu
+#            zhiwei:dwmc/zwmc/leixing/rs/kcxh/msdate
+#            mianshishi:kcxh/mssinfo/hksinfo
 #zhiwei表中的数据是脚本根据基本考生数据，计算得出考场序号后一一填入数据库中
 #执行脚本后SELECT ksinfo.*,zhiwei.kcxh FROM ksinfo,zhiwei where ksinfo.bkdw=zhiwei.dwmc and ksinfo.bkzw=zhiwei.zwmc order by kcxh;即可得出每个考生的考场分配
 import sys      
 reload(sys)    
 sys.setdefaultencoding('utf8')  
 import MySQLdb
-maxrs=30
-
+#------------------------------------------------全局变量设置区域--------------------------------------------------------------------
+maxrs=30 #每个考场设置人数
+maxkcxh=40   #考场序号初始1
+msdate=1
 conn= MySQLdb.connect( host='192.168.1.107', port = 3306, user='root', passwd='123123', db ='mianshi',charset='utf8')
 cur = conn.cursor()
+#------------------------------------------------全局变量设置区域--------------------------------------------------------------------
 class zhiwei(object):
     def __init__(self,dwmc,zwmc,leixing,rs):
         self.dwmc=dwmc
@@ -22,34 +26,39 @@ class zhiwei(object):
         self.leixing=leixing
         self.rs=rs
 
-def Initzhiwei(zhiweilist):
+def Initzhiwei(leixing,zhiweilist):
     #这里从数据库读取职位形成列表
-    query="SELECT bkdw,bkzw,leixing,count(*) FROM ksinfo where leixing='公务员' group by bkdw,bkzw ;" 
+    #zhiweilist=[] #清空列表
+    zhiweilist.append(zhiwei('null','null','null',0))#插入第一条空数据，物品0不存在
+    query="SELECT bkdw,bkzw,leixing,count(*) FROM ksinfo where leixing='%s' group by bkdw,bkzw ;" % (leixing)
+    print query
     cur.execute(query)
     zw=cur.fetchall()
-    zhiweilist.append(zhiwei('null','null','null',0))#插入第一条空数据，物品0不存在
     for item in zw:
         zhiweilist.append(zhiwei(item[0],item[1],item[2],item[3]))
 
-def arrange_kc(zhiweilist):
-    i=0
+def arrange_kc(kcxh,zhiweilist):
     w=[]
     v=[]
     for item in zhiweilist:
         w.append(item.rs)
-    cur.execute("truncate table zhiwei")
-    conn.commit()
     while len(zhiweilist)!=0:#结束条件为职位列表为空，即所有职位均分配到考场中
         tmp=[]
         res=bag(w,w,len(w),maxrs)
         sol=show(maxrs,w,res)
         sum=0
-        if len(sol)==0:
+        if len(sol)==0: #解数组中没有解就跳出循环
             break
-        print u"------------------------第",i,u"考场-----------------"
+        print u"------------------------第",kcxh,u"考场-----------------"
+        #import pdb; pdb.set_trace()
+        msdate=1
+        thiskcxh=kcxh
+        if kcxh>maxkcxh:
+            thiskcxh=kcxh-maxkcxh
+            msdate=2
         for item in sol:
             tmp.append(zhiweilist[item])
-            query="insert into zhiwei (`dwmc`, `zwmc`, `leixing`, `rs`, `kcxh`) values('%s','%s','%s',%s,%s)" % (zhiweilist[item].dwmc,zhiweilist[item].zwmc,zhiweilist[item].leixing,zhiweilist[item].rs,i+1)
+            query="insert into zhiwei (`dwmc`, `zwmc`, `leixing`, `rs`, `kcxh`,`msdate`) values('%s','%s','%s',%s,%s,%s)" % (zhiweilist[item].dwmc,zhiweilist[item].zwmc,zhiweilist[item].leixing,zhiweilist[item].rs,thiskcxh,msdate)
             cur.execute(query)
             print query
             sum=zhiweilist[item].rs+sum
@@ -57,9 +66,9 @@ def arrange_kc(zhiweilist):
         for item in sol:
             del w[item]
             del zhiweilist[item]
-        i=i+1
+        kcxh=kcxh+1
         conn.commit()
-    conn.close()
+    return kcxh
 #---------------------------------------------------------------------------------------------------------------------------
 #核心算法01背包来做，懒得改进算法，当质量数组和价值数组为同一数组就是我要的结果
 #务必注意质量数组中是从第一个物品开始计算的，第0个物品和正常人的思维有冲突，故数组第0单元不要填入数据。
@@ -88,15 +97,13 @@ def show(c,w,res): #c背包容量，w重量数组，res状态数组
         i=i-1
     return sol #sol数组存放了最优解的物品序号
 #---------------------------------------------------------------------------------------------------------------------------
-def sol_terminal():
-    result=open('./result.txt')
-    query='SELECT ksinfo.*,zhiwei.kcxh FROM ksinfo,zhiwei where ksinfo.bkdw=zhiwei.dwmc and ksinfo.bkzw=zhiwei.zwmc order by kcxh;'
-    cur.execute(query)
-    info=cur.fetchall()
-    for item in info:
-        print item[0], item[1], item[2], item[3],item[4], item[5], item[6] 
 if __name__ == "__main__":
-    zhiweilist=[]
-    Initzhiwei(zhiweilist)
-    arrange_kc(zhiweilist)
-    #sol_terminal()
+    leixinglist=['公务员','警察','选调生']
+    kcxh=1
+    cur.execute("truncate table zhiwei")
+    conn.commit()
+    for leixing in leixinglist:
+        zhiweilist=[] #复用数组每次都要记得清空
+        Initzhiwei(leixing,zhiweilist)
+        kcxh=arrange_kc(kcxh,zhiweilist)
+    conn.close()
